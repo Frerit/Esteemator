e9f79743044add9c6628be5777819284ef0be227
usuario <julianperez@strategee.us>
2017-04-06 10:01:51 -0500

    Instalación de Librerías

--- Esteemator-Pro/Esteemator-Pro/View/HomePageViewController.swift
+++ Esteemator-Pro/Esteemator-Pro/View/HomePageViewController.swift
@@ -8,42 +8,98 @@
 
 import UIKit
 
-class HomePageViewController: UIViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
-   
+class HomePageViewController: UIViewController, UIPageViewControllerDataSource {
     
-    var pageViewController: UIPageViewController!
-    let pages = ["FirstPageVC","SecondPageVC","ThrirdPageVC"]
+    var pageController: UIPageViewController!
+    var pageTitles: NSArray!
+    var pageImage: NSArray!
+    
+    @IBOutlet weak var butonRegister: UIButton!
+    @IBOutlet weak var butonLogin: UIButton!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let vc  = storyboard?.instantiateViewController(withIdentifier: "PageviewControllers") {
-            self.addChildViewController(vc)
-            self.view.addSubview(vc.view)
-            
-            self.pageViewController.dataSource = self
-            self.pageViewController.delegate = self
-            
-            pageViewController.setViewControllers([self.viewcontrollerAtOIndex(index: 0)!], direction: .forward, animated: true, completion: nil)
-            pageViewController.didMove(toParentViewController: self)
-        }
+        self.pageTitles = NSArray(objects: "Primero","Second","Tercero")
+        self.pageImage = NSArray(objects: "01","02","03")
+        
+        self.pageController = self.storyboard?.instantiateViewController(withIdentifier: "PageViewController") as! UIPageViewController
+        
+        self.pageController.dataSource = self
+        
+        let starVC = self.viewControllerAtIndex(index: 0) as  ContentPageViewController
+        let viewController = NSArray(object: starVC)
+        
+        self.pageController.setViewControllers(viewController as? [UIViewController], direction: .forward, animated: true, completion: nil)
+        self.pageController.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height - 60)
+        
+        self.addChildViewController(self.pageController)
+        self.view.addSubview(self.pageController.view)
+        self.pageController.didMove(toParentViewController: self)
+        
+        self.butonLogin.layer.cornerRadius = 3
+        self.butonRegister.layer.cornerRadius = 3
+        
     }
-
-    func viewcontrollerAtOIndex(index: Int) -> UIViewController? {
-        let vc = storyboard?.instantiateViewController(withIdentifier: pages[index])
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func viewControllerAtIndex(index: Int) -> ContentPageViewController {
+        if ((self.pageTitles.count == 0 ) || (index >= self.pageTitles.count)) {
+            return ContentPageViewController()
+        }
+        
+        let vc: ContentPageViewController = self.storyboard?.instantiateViewController(withIdentifier: "ContentViewController") as! ContentPageViewController
+        
+        vc.imagefile = self.pageImage[index] as! String
+        vc.titleIndex = self.pageTitles[index] as! String
+        vc.pageIndex = index
+        
         return vc
     }
     
     
-    
-    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        return nil
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        let vc = viewController as! ContentPageViewController
+        var index = vc.pageIndex as Int
+        
+        if (index == 0 || index == NSNotFound) { return nil }
+        index -= 1
+        
+        return self.viewControllerAtIndex(index: index)
     }
     
-    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        return nil
+    
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        let vc = viewController as! ContentPageViewController
+        var index = vc.pageIndex as Int
+        
+        if (index == NSNotFound) {   return nil  }
+        
+        index += 1
+        if ( index == self.pageTitles.count) {  return nil }
+        
+        return self.viewControllerAtIndex(index: index)
     }
-
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return self.pageTitles.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+    
     
     
 }
